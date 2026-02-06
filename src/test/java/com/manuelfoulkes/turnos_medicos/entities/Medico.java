656cb092a89b3faff8a12566d4a56d4f0b30f229
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-02-06 20:11:07 -0300

    src/test/java/com/manuelfoulkes/turnos_medicos/entities/Medico.java

---
+++ src/test/java/com/manuelfoulkes/turnos_medicos/entities/Medico.java
@@ -0,0 +1,29 @@
+package com.manuelfoulkes.turnos_medicos.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "medico")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Builder
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "apellido", nullable = false)
+    private String apellido;
+
+    @Column(name = "matricula", nullable = false)
+    private String matricula;
+}
