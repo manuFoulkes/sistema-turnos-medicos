4a4843eea184bcd89a43b6c2057dcc98deed02f7
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-02-06 19:13:31 -0300

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/manuelfoulkes/turnos_medicos/TurnosMedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuelfoulkes.turnos_medicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosMedicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
