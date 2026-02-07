520205e9b5a4d9e4b9be616ae6014b5509cab35c
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-02-07 16:11:22 -0300

    feat: add TurnoRepository Interface

---
+++ src/main/java/com/manuelfoulkes/turnos_medicos/repositories/TurnoRepository.java
@@ -0,0 +1,7 @@
+package com.manuelfoulkes.turnos_medicos.repositories;
+
+import com.manuelfoulkes.turnos_medicos.entities.Turno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurnoRepository extends JpaRepository<Turno, Long> {
+}
