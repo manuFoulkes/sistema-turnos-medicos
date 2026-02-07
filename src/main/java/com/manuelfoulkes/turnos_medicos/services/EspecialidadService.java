f4f339365cd447c308afebf27803394eef89b712
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-02-07 17:45:17 -0300

    feat: add EspecialidadService with basic operations methods

---
+++ src/main/java/com/manuelfoulkes/turnos_medicos/services/EspecialidadService.java
@@ -0,0 +1,37 @@
+package com.manuelfoulkes.turnos_medicos.services;
+
+import com.manuelfoulkes.turnos_medicos.entities.Especialidad;
+import com.manuelfoulkes.turnos_medicos.repositories.EspecialidadRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EspecialidadService {
+
+    private final EspecialidadRepository especialidadRepository;
+
+    public Especialidad crearEspecialidad(Especialidad especialidad) {
+        return especialidadRepository.save(especialidad);
+    }
+
+    public Especialidad getEspecialidadById(Long id) {
+        return especialidadRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Especialidad no encontrada"));
+    }
+
+    public List<Especialidad> getAllEspecialidades() {
+        return especialidadRepository.findAll();
+    }
+
+    public Especialidad updateEspecialidad(Long id, String nombre) {
+        Especialidad especialidad = especialidadRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Especialidad no encontrada"));
+
+        especialidad.setNombre(nombre);
+        
+        return especialidadRepository.save(especialidad);
+    }
+}
