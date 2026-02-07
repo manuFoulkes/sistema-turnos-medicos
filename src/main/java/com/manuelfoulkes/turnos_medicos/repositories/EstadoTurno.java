49fa30d4969d288c5364f783905181ee6285da28
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-02-07 15:56:27 -0300

    feat: add EstadoTurnoRepository Interface

---
+++ src/main/java/com/manuelfoulkes/turnos_medicos/repositories/EstadoTurno.java
@@ -0,0 +1,6 @@
+package com.manuelfoulkes.turnos_medicos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoTurno extends JpaRepository<EstadoTurno, Long> {
+}
