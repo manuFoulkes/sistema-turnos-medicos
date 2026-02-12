f9f1903a01686beeb9dbb593d7826f1ed18b4406
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-02-12 19:57:45 -0300

    feat: add findByDni method to PacienteRepository

--- src/main/java/com/manuelfoulkes/turnos_medicos/repositories/PacienteRepository.java
+++ src/main/java/com/manuelfoulkes/turnos_medicos/repositories/PacienteRepository.java
@@ -3,5 +3,9 @@ package com.manuelfoulkes.turnos_medicos.repositories;
 import com.manuelfoulkes.turnos_medicos.entities.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    Optional<Paciente> findByDni(String dni);
 }
