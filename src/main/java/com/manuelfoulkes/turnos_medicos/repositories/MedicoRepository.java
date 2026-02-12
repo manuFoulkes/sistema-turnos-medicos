c32b5c7a31f8cd5c0cb483966482b48f48859933
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-02-12 17:58:58 -0300

    feat: add findByMatricula method on MedicoRepository

--- src/main/java/com/manuelfoulkes/turnos_medicos/repositories/MedicoRepository.java
+++ src/main/java/com/manuelfoulkes/turnos_medicos/repositories/MedicoRepository.java
@@ -2,6 +2,11 @@ package com.manuelfoulkes.turnos_medicos.repositories;
 
 import com.manuelfoulkes.turnos_medicos.entities.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    Optional<Medico> findByMatricula(String matricula);
 }
