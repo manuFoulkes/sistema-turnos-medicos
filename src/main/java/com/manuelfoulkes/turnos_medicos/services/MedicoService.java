800230335733df36c434622f5c4a4aaf78171597
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-02-12 18:05:14 -0300

    refactor: return MedicoResponseDTO in crearMedico method and update matricula validation

--- src/main/java/com/manuelfoulkes/turnos_medicos/services/MedicoService.java
+++ src/main/java/com/manuelfoulkes/turnos_medicos/services/MedicoService.java
@@ -1,6 +1,8 @@
 package com.manuelfoulkes.turnos_medicos.services;
 
 import com.manuelfoulkes.turnos_medicos.dtos.requests.MedicoRequestDTO;
+import com.manuelfoulkes.turnos_medicos.dtos.responses.EspecialidadResponseDTO;
+import com.manuelfoulkes.turnos_medicos.dtos.responses.MedicoResponseDTO;
 import com.manuelfoulkes.turnos_medicos.entities.Especialidad;
 import com.manuelfoulkes.turnos_medicos.entities.Medico;
 import com.manuelfoulkes.turnos_medicos.repositories.EspecialidadRepository;
@@ -17,25 +19,39 @@ public class MedicoService {
     private final MedicoRepository medicoRepository;
     private final EspecialidadRepository especialidadRepository;
 
-    public Medico crearMedico(MedicoRequestDTO medicoRequestDTO) {
-        String numeroMatricula = medicoRequestDTO.matricula();
+    public MedicoResponseDTO crearMedico(MedicoRequestDTO medicoRequestDTO) {
+        String matricula = medicoRequestDTO.matricula();
 
-        medicoRepository.getMedicoByNumeroMatricula(numeroMatricula)
-                .ifPresent(() -> {
-                    throw new RuntimeException("El médico con la matrícula " + numeroMatricula + " ya existe");
-                });
+        if (medicoRepository.findByMatricula(matricula).isPresent()) {
+            throw new RuntimeException("El medico ya existe");
+        }
 
         Long especialidadId = medicoRequestDTO.especialidadId();
+
         Especialidad especialidad = especialidadRepository.findById(especialidadId)
                 .orElseThrow(() -> new RuntimeException("La especialidad con Id " + especialidadId + " no existe"));
 
+        EspecialidadResponseDTO especialidadResponseDTO = new EspecialidadResponseDTO(
+                especialidad.getId(),
+                especialidad.getNombre()
+        );
+
         Medico nuevoMedico = new Medico();
+
         nuevoMedico.setNombre(medicoRequestDTO.nombre());
         nuevoMedico.setApellido(medicoRequestDTO.apellido());
         nuevoMedico.setMatricula(medicoRequestDTO.matricula());
         nuevoMedico.setEspecialidad(especialidad);
 
-        return medicoRepository.save(nuevoMedico);
+        nuevoMedico = medicoRepository.save(nuevoMedico);
+
+        return new MedicoResponseDTO(
+                nuevoMedico.getId(),
+                nuevoMedico.getNombre(),
+                nuevoMedico.getApellido(),
+                nuevoMedico.getMatricula(),
+                especialidadResponseDTO
+        );
     }
 
     public Medico getMedicoById(Long id) {
