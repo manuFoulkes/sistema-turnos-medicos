35da51ae0e4ab19a393292c30e56f31c0d889f0a
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-02-12 12:02:30 -0300

    refactor: update crearMedico method in MedicoService to use MedicoRequestDTO and inject EspecialidadRepository

--- src/main/java/com/manuelfoulkes/turnos_medicos/services/MedicoService.java
+++ src/main/java/com/manuelfoulkes/turnos_medicos/services/MedicoService.java
@@ -1,6 +1,9 @@
 package com.manuelfoulkes.turnos_medicos.services;
 
+import com.manuelfoulkes.turnos_medicos.dtos.requests.MedicoRequestDTO;
+import com.manuelfoulkes.turnos_medicos.entities.Especialidad;
 import com.manuelfoulkes.turnos_medicos.entities.Medico;
+import com.manuelfoulkes.turnos_medicos.repositories.EspecialidadRepository;
 import com.manuelfoulkes.turnos_medicos.repositories.MedicoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +15,27 @@ import java.util.List;
 public class MedicoService {
 
     private final MedicoRepository medicoRepository;
+    private final EspecialidadRepository especialidadRepository;
 
-    public Medico crearMedico(Medico medico) {
-        return medicoRepository.save(medico);
+    public Medico crearMedico(MedicoRequestDTO medicoRequestDTO) {
+        String numeroMatricula = medicoRequestDTO.matricula();
+
+        medicoRepository.getMedicoByNumeroMatricula(numeroMatricula)
+                .ifPresent(() -> {
+                    throw new RuntimeException("El médico con la matrícula " + numeroMatricula + " ya existe");
+                });
+
+        Long especialidadId = medicoRequestDTO.especialidadId();
+        Especialidad especialidad = especialidadRepository.findById(especialidadId)
+                .orElseThrow(() -> new RuntimeException("La especialidad con Id " + especialidadId + " no existe"));
+
+        Medico nuevoMedico = new Medico();
+        nuevoMedico.setNombre(medicoRequestDTO.nombre());
+        nuevoMedico.setApellido(medicoRequestDTO.apellido());
+        nuevoMedico.setMatricula(medicoRequestDTO.matricula());
+        nuevoMedico.setEspecialidad(especialidad);
+
+        return medicoRepository.save(nuevoMedico);
     }
 
     public Medico getMedicoById(Long id) {
@@ -33,7 +54,7 @@ public class MedicoService {
     public void deleteMedico(Long id) {
         Medico medico = medicoRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Medico con ID " + id + " no encontrado"));
-        
+
         medicoRepository.delete(medico);
     }
 }
