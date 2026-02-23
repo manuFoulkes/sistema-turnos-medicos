0be8514c65a34d85c81606a3c7abe6aa35624566
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-02-23 16:45:57 -0300

    feat: implement existeTurno and getTurnosActivos methods in TurnoService. delete comments in registrarTurno method

--- src/main/java/com/manuelfoulkes/turnos_medicos/services/TurnoService.java
+++ src/main/java/com/manuelfoulkes/turnos_medicos/services/TurnoService.java
@@ -22,40 +22,32 @@ public class TurnoService {
     private final PacienteRepository pacienteRepository;
     private final MedicoRepository medicoRepository;
 
-    // TODO: Revisar y limpiar. Sacar funcionalidades a métodos privados
+    // TODO: Implementar mappers
     public TurnoResponseDTO registrarTurno(TurnoRequestDTO turnoRequest) {
-        // TurnoRequestDTO viene con: fechaHora, pacienteId, medicoId
         Long pacienteId = turnoRequest.pacienteId();
         Long medicoId = turnoRequest.medicoId();
+        int maximoTurnos = 3;
 
-        // 1 - Validar que el paciente existe
         Paciente paciente = pacienteRepository.findById(pacienteId)
                 .orElseThrow(() -> new RuntimeException("Paciente no encontrado"));
 
-        // 2 - Validar que el medico existe
         Medico medico = medicoRepository.findById(medicoId)
                 .orElseThrow(() ->  new RuntimeException("Medico no encontrado"));
 
-        // 3 - Validar que el horario sea posterior a ahora
         if(turnoRequest.fechaHora().isBefore(LocalDateTime.now())) {
             throw new RuntimeException("Fecha inválida");
         }
 
-        // TODO: Crear existeTurno()
-        // 4 - Validar que no exista ya un turno asignado en ese horario con ese medico
-        if(!existeTurno(medicoId, turnoRequest.fechaHora())) {
+        if(existeTurno(medicoId, turnoRequest.fechaHora(), EstadoTurno.CANCELADO)) {
             throw new RuntimeException("El medico ya tiene un turno asignado en ese horario");
         }
 
-        // TODO: Crear getTurnosActivos()
-        // 5 - Validar límite de turnos activos del paciente
-        int turnosActivos = getTurnosActivos(pacienteId, EstadoTurno.CANCELADO, LocalDateTime.now());
+        int turnosActivos = getTurnosActivos(pacienteId, EstadoTurno.RESERVADO, LocalDateTime.now());
 
-        if (turnosActivos > 3) {
+        if (turnosActivos > maximoTurnos) {
             throw new RuntimeException("Cantidad de turnos por paciente excedida");
         }
 
-        // 6 - Crear el turno
         Turno nuevoTurno =  new Turno();
         nuevoTurno.setFechaHora(turnoRequest.fechaHora());
         nuevoTurno.setPaciente(paciente);
@@ -63,7 +55,6 @@ public class TurnoService {
 
         nuevoTurno = turnoRepository.save(nuevoTurno);
 
-        // 7 - Crear PacienteResponseDTO
         PacienteResponseDTO pacienteResponse = new PacienteResponseDTO(
                 paciente.getId(),
                 paciente.getNombre(),
@@ -73,7 +64,6 @@ public class TurnoService {
                 paciente.getTelefono()
         );
 
-        // 8 - Crear EspecialidadResponseDTO y MedicoResponseDTO
         Especialidad especialidad = medico.getEspecialidad();
 
         EspecialidadResponseDTO especialidadResponse = new EspecialidadResponseDTO(
@@ -89,7 +79,6 @@ public class TurnoService {
                 especialidadResponse
         );
 
-        // 9 - Mapear a TurnoResponseDTO y retornarlo
         return new TurnoResponseDTO(
                 nuevoTurno.getId(),
                 nuevoTurno.getFechaHora(),
@@ -98,5 +87,13 @@ public class TurnoService {
                 medicoResponse,
                 nuevoTurno.getFechaCreacion()
         );
+    }
+
+    private boolean existeTurno(Long medicoId, LocalDateTime fechaHora, EstadoTurno estadoTurno) {
+        return turnoRepository.existsByMedicoIdAndFechaHoraAndEstadoNot(medicoId, fechaHora, estadoTurno);
+    }
+
+    private int getTurnosActivos(Long pacienteId, EstadoTurno estadoTurno, LocalDateTime fechaHora) {
+        return turnoRepository.countByPacienteIdAndEstadoAndFechaHoraAfter(pacienteId, estadoTurno, fechaHora);
     }
 }
