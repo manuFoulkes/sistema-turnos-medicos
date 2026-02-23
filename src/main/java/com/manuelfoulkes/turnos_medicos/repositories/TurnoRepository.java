4a60ccbeaa9a1fb0fbbab7ad5a4bd4654e62bee7
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-02-23 16:48:34 -0300

    feat: implement two custom query methods to TurnoRepository

--- src/main/java/com/manuelfoulkes/turnos_medicos/repositories/TurnoRepository.java
+++ src/main/java/com/manuelfoulkes/turnos_medicos/repositories/TurnoRepository.java
@@ -1,8 +1,14 @@
 package com.manuelfoulkes.turnos_medicos.repositories;
 
+import com.manuelfoulkes.turnos_medicos.entities.EstadoTurno;
 import com.manuelfoulkes.turnos_medicos.entities.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface TurnoRepository extends JpaRepository<Turno, Long> {
 
+    boolean existsByMedicoIdAndFechaHoraAndEstadoNot(Long medicoId, LocalDateTime fechaHora, EstadoTurno estado);
+
+    int countByPacienteIdAndEstadoAndFechaHoraAfter(Long pacienteId, EstadoTurno estado, LocalDateTime fechaHora);
 }
