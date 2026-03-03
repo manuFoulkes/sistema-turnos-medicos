4b004412fc4baf1329415ef47f2ce76624e54531
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-03-03 18:37:36 -0300

    refactor: rename and translate PacienteService -> PatientService

---
+++ src/main/java/com/manuelfoulkes/turnos_medicos/services/PatientService.java
@@ -0,0 +1,109 @@
+package com.manuelfoulkes.turnos_medicos.services;
+
+import com.manuelfoulkes.turnos_medicos.dtos.requests.PatientRequestDTO;
+import com.manuelfoulkes.turnos_medicos.dtos.responses.PatientResponseDTO;
+import com.manuelfoulkes.turnos_medicos.entities.Patient;
+import com.manuelfoulkes.turnos_medicos.repositories.PatientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequest) {
+        String nationalId = patientRequest.nationalId();
+
+        if(patientRepository.findByNationalId(nationalId).isPresent()) {
+            throw new RuntimeException("El paciente ya está registrado");
+        }
+
+        Patient newPatient = new Patient();
+
+        newPatient.setName(patientRequest.name());
+        newPatient.setLastName(patientRequest.lastName());
+        newPatient.setNationalId(patientRequest.nationalId());
+        newPatient.setEmail(patientRequest.email());
+        newPatient.setPhoneNumber(patientRequest.phoneNumber());
+
+        newPatient = patientRepository.save(newPatient);
+
+        return new PatientResponseDTO(
+                newPatient.getId(),
+                newPatient.getName(),
+                newPatient.getLastName(),
+                newPatient.getNationalId(),
+                newPatient.getEmail(),
+                newPatient.getPhoneNumber()
+        );
+    }
+
+    public PatientResponseDTO getById(Long id) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Paciente no encontrado"));
+
+        return new PatientResponseDTO(
+                patient.getId(),
+                patient.getName(),
+                patient.getLastName(),
+                patient.getNationalId(),
+                patient.getEmail(),
+                patient.getPhoneNumber()
+        );
+    }
+
+    public List<PatientResponseDTO> getAllPatients() {
+        List<Patient> patients = patientRepository.findAll();
+
+        List<PatientResponseDTO> pacientesResponse = new ArrayList<>();
+
+        for (Patient p : patients) {
+            PatientResponseDTO pacienteResponse = new PatientResponseDTO(
+                    p.getId(),
+                    p.getName(),
+                    p.getLastName(),
+                    p.getNationalId(),
+                    p.getEmail(),
+                    p.getPhoneNumber()
+            );
+
+            pacientesResponse.add(pacienteResponse);
+        }
+
+        return pacientesResponse;
+    }
+
+    public PatientResponseDTO updatePatient(Long id, PatientRequestDTO patientRequest) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Paciente no encontrado"));
+
+        patient.setName(patientRequest.name());
+        patient.setLastName(patientRequest.lastName());
+        patient.setNationalId(patientRequest.nationalId());
+        patient.setEmail(patientRequest.email());
+        patient.setPhoneNumber(patientRequest.phoneNumber());
+
+        Patient newPatient = patientRepository.save(patient);
+
+        return new PatientResponseDTO(
+                newPatient.getId(),
+                newPatient.getName(),
+                newPatient.getLastName(),
+                newPatient.getNationalId(),
+                newPatient.getEmail(),
+                newPatient.getPhoneNumber()
+        );
+    }
+
+    public void deletePatient(Long id) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Paciente no encontrado"));
+
+        patientRepository.delete(patient);
+    }
+}
