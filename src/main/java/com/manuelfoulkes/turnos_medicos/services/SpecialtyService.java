a4299509578c2a30fe28e6fb0f1129fa8b2860f5
Manuel Foulkes <manuel.foulkes90@gmail.com>
2026-03-04 15:34:39 -0300

    refactor: implement DTOs in SpecialtyService

--- src/main/java/com/manuelfoulkes/turnos_medicos/services/SpecialtyService.java
+++ src/main/java/com/manuelfoulkes/turnos_medicos/services/SpecialtyService.java
@@ -1,10 +1,13 @@
 package com.manuelfoulkes.turnos_medicos.services;
 
+import com.manuelfoulkes.turnos_medicos.dtos.requests.SpecialtyRequestDTO;
+import com.manuelfoulkes.turnos_medicos.dtos.responses.SpecialtyResponseDTO;
 import com.manuelfoulkes.turnos_medicos.entities.Specialty;
 import com.manuelfoulkes.turnos_medicos.repositories.SpecialtyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,25 +16,42 @@ public class SpecialtyService {
 
     private final SpecialtyRepository specialtyRepository;
 
-    public Specialty createSpecialty(Specialty specialty) {
-        return specialtyRepository.save(specialty);
+    // TODO: Refactor: Usar DTOs en todos los métodos
+    public SpecialtyResponseDTO createSpecialty(SpecialtyRequestDTO specialtyRequest) {
+        Specialty specialty = new Specialty();
+        specialty.setName(specialtyRequest.name());
+
+        Specialty newSpecialty = specialtyRepository.save(specialty);
+        return new SpecialtyResponseDTO(newSpecialty.getId(), newSpecialty.getName());
     }
 
-    public Specialty getSpecialtyById(Long id) {
-        return specialtyRepository.findById(id)
+    public SpecialtyResponseDTO getSpecialtyById(Long id) {
+        Specialty specialty = specialtyRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Especialidad no encontrada"));
+
+        return new SpecialtyResponseDTO(specialty.getId(), specialty.getName());
     }
 
-    public List<Specialty> getAllSpecialties() {
-        return specialtyRepository.findAll();
+    public List<SpecialtyResponseDTO> getAllSpecialties() {
+        List<Specialty> specialties = specialtyRepository.findAll();
+        List<SpecialtyResponseDTO> specialtiesResponse = new ArrayList<>();
+
+        for(Specialty specialty : specialties) {
+            SpecialtyResponseDTO specialtyResponse = new SpecialtyResponseDTO(specialty.getId(), specialty.getName());
+            specialtiesResponse.add(specialtyResponse);
+        }
+
+        return specialtiesResponse;
     }
 
-    public Specialty updateEspecialidad(Long id, String name) {
+    public SpecialtyResponseDTO updateEspecialidad(SpecialtyRequestDTO specialtyRequestDTO) {
         Specialty specialty = specialtyRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Especialidad no encontrada"));
 
         specialty.setName(name);
 
-        return specialtyRepository.save(specialty);
+        Specialty updatedSpecialty = specialtyRepository.save(specialty);
+
+        return new SpecialtyResponseDTO(updatedSpecialty.getId(), updatedSpecialty.getName());
     }
 }
